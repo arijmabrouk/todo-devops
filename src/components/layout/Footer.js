53a920ecdef67fdc1d6d92f949ec3c966af8bc4a
Mabrouk Arij <arijmabrouk@neopolis-dev.com>
2024-05-24 11:59:05 +0100

    add footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer style={headerStyle}>
+      <h1>Mabrouk Arij 2GL2</h1>
+    </footer>
+  )
+}
+
+const headerStyle = {
+    background: '#ffa500',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+  }
+
+export default Footer
